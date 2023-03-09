5fa8facb756948bd4337f1743d8580e607dde18a
suso51 <98268213+suso51@users.noreply.github.com>
2023-03-09 19:20:58 +0100

    Add files via upload

---
+++ radek556.pl/mplayer/bio/js/4363-js-commands.js
@@ -0,0 +1,99 @@
+var youtube = "https://www.youtube.com/@radek5569";
+var password = "radek556";
+var instagram = "not now...";
+var github = "not now...";
+
+whois = [
+  "<br>",
+  "Hey, I'm Radek / Michal!👋",
+  "I am 22 years old.",
+  "I am from city Gdańsk, trying to improve myself in software programming and voice acting. Coding is a piece of cake. 😊",
+  "Programmer and voice actor of sum Games / Videos etc. Trying some fandubs and other things to improve skills.",
+  "Don't forget using 2FA 😉",
+  "My Aim: Senior Developer & Fully Voice Actor.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/radek556' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">not now...' + '</a>',
+  'github         <a href="' + github + '" target="_blank">not now...' + "</a>",
+  'discord         <a href="' + discord + '" target="_blank">okokok#1362' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+donation = [
+  "<br>",
+  '<span class="command">Hey thanks for thinking about donation 💖</span>',
+  '<a href="https://www.paypal.me/radek556" target="_blank">Click For Paypal</a>',
+  '<a href="https://www.buymeacoffee.com/radek556" target="_blank">Click For Buy Me A Coffe</a>',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Most projects are private on my Github - SOON.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is okokok?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">donate</span>         Donation Link',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+
+banner = [
+    "                          oooo$$$$$$$$$$$$oooo",
+    "                      oo$$$$$$$$$$$$$$$$$$$$$$$$o",
+    "                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$",
+    "   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$",
+    "oo $ $ \"$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$",
+    "\"$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$",
+    "  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$",
+    "  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  \"\"\"$$$",
+    "   \"$$$\"\"\"\"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$",
+    "    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$o",
+    "   o$$\"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o",
+    "   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\" \"$$$$$$ooooo$$$$o",
+    "  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$",
+    "  $$$$$$$$\"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$\"\"\"\"\"\"\"\"",
+    " \"\"\"\"       $$$$    \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"      o$$$",
+    "            \"$$$o     \"\"\"$$$$$$$$$$$$$$$$$$\"$$\"         $$$",
+    "              $$$o          \"$$\"\"$$$$$$\"\"\"\"           o$$$",
+    "               $$$$o                 oo             o$$$\"",
+    "                \"$$$$o      o$$$$$$o\"$$$$o        o$$$$",
+    "                  \"$$$$$oo     \"\"$$$$o$$$$$o   o$$$$\"\"  ",
+    "                     \"\"$$$$$oooo  \"$$$o$$$$$$$$$\"\"\"",
+    "                        \"\"$$$$$$$oo $$$$$$$$$$       ",
+    "                                \"\"\"\"$$$$$$$$$$$        ",
+    "                                    $$$$$$$$$$$$       ",
+    "                                     $$$$$$$$$$\"      ",
+    "                                      \"$$$\"\"\"\"",
+    "                                                             ",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+]
