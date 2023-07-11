e2133a05330a4e366cab14c55aa7d808f5b85b71
suso51 <98268213+suso51@users.noreply.github.com>
2023-07-11 17:10:05 +0200

    Update 4363-js-commands.js

--- radek556.pl/mplayer/bio/js/4363-js-commands.js
+++ radek556.pl/mplayer/bio/js/4363-js-commands.js
@@ -50,10 +50,44 @@ projects = [
   "<br>"
 ];
 
+voiceactor = [
+  "<br>",
+  "Commands about voice acting:",
+  '<span class="command">price</span>          Why price is so big? & How to Ask me for price?',
+  '<span class="command">voiceacting</span>          What is my voice acting projects?',
+  "<br>",
+];
+
+price = [
+  "<br>",
+  "U pay for 'service', not for a product, that means u need to pay price for my time, and professionalism. Like with doctor or mechanic, u pay for his service because he knows best what u need",
+  "<br>",
+  " (e.g. he can repair your hand within 5 minutes or engine within 10 minutes but u pay for his job done not for 'how many times he hit engine with hammer' etc.)",
+  "<br>".
+  "U pay mainly for:   Diction Training, Voice Emission Training, Training Working with Microphone (yes u need that too), that's all years of learning and training. - We need to do that EVERYDAY.",
+  "<br>",
+  "U pay for making my portfolio, my experience. If u found me u just could consider that my voice and style is good, for my interpretations. That's not generic things like buying Polish hammer or buying Chinese hammer - because it would be the same",
+  "<br>",
+  "U pay for my Recording studio. Without it u can't have good voice actor. It's not only talking to shitty microphone.",
+  "<br>",
+  "U pay for mu time, it's not like u pay for 5 minutes of recording and I or other guys make it in 5 minutes, we need much longer time to record it perfectly. I need to postproduct it, master it, mix it, and u will get READY product just to upload it anywhere. 1 Hour recording commonly have same preparations like 5 minutes of recording.",
+  "<br>",
+  "U pay for my money that got spent on tutorials, lessons, everything for making my skills even better. For my time that teached me using all shit in softwares.",
+  "IMPORTANT: U PAY FOR MY COPYRIGHT!",
+];
+
+voiceacting = [
+  "<br>",
+  "I don't want to give u much much projects that i gave my voice to, there's just some examples:",
+  "Gothic 2 NoTR - Lobos",
+  "Gothic 1 - Edycja Rozszerzona",
+];
+
 help = [
   "<br>",
   '<span class="command">whois</span>          Who is okokok?',
   '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">voiceactor</span>         Voice Acting things',
   '<span class="command">video</span>          View YouTube videos',
   '<span class="command">donate</span>         Donation Link',
   '<span class="command">social</span>         Display social networks',
